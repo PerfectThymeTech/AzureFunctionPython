fdc204ce8accdbf9504d80af93bf211a3c47219d
Marvin Buss <marvin.buss@gmail.com>
2023-06-09 21:21:23 +0200

    Update path in test

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ def client() -> TestClient:
 @pytest.mark.parametrize("version", ("v1",))
 def test_get_heartbeat(client, version):
     # arrange
-    path = f"/corrector/{version}/heartbeat"
+    path = f"/health/{version}/heartbeat"
 
     # action
     response = client.get(path)
