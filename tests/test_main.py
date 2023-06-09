7a87ccccc6a6c277cdd6a5e18b72586a5ed19562
Marvin Buss <marvin.buss@gmail.com>
2023-06-09 16:57:42 +0200

    Add python test

---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from code.function.core.config import settings
+from code.function.main import app
+
+
+@pytest.fixture(scope="module")
+def client() -> TestClient:
+    return TestClient(app)
+
+
+@pytest.mark.parametrize("version", ("v1",))
+def test_get_heartbeat(client, version):
+    # arrange
+    path = f"/corrector/{version}/heartbeat"
+
+    # action
+    response = client.get(path)
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {"isAlive": True}
