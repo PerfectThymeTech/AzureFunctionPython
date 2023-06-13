fed3c9d70d56dc3b51cdedb6eb4935d2476d087f
Marvin Buss <marvin.buss@gmail.com>
2023-06-13 12:37:36 +0200

    Add function json

--- code/function/function_app.py
+++ code/function/function_app.py
@@ -34,5 +34,5 @@ app = func.AsgiFunctionApp(
 )
 
 
-# async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-#     return await func.AsgiMiddleware(fastapi_app).handle_async(req, context)
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(fastapi_app).handle_async(req, context)
