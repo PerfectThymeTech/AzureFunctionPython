32786755b4215f686cb82ffd3242f884b25043a2
Marvin Buss <marvin.buss@gmail.com>
2023-06-14 17:35:36 +0200

    Add role assignments for app insights

--- code/infra/logging.tf
+++ code/infra/logging.tf
@@ -10,7 +10,7 @@ resource "azurerm_application_insights" "application_insights" {
   force_customer_storage_for_profiler   = false
   internet_ingestion_enabled            = true
   internet_query_enabled                = true
-  local_authentication_disabled         = false
+  local_authentication_disabled         = true
   retention_in_days                     = 90
   sampling_percentage                   = 100
   workspace_id                          = azurerm_log_analytics_workspace.log_analytics_workspace.id
--- code/infra/roleassignments.tf
+++ code/infra/roleassignments.tf
@@ -9,3 +9,9 @@ resource "azurerm_role_assignment" "function_role_assignment_key_vault" {
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azapi_resource.function.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "function_role_assignment_application_insights" {
+  scope                = azurerm_application_insights.application_insights.id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = azapi_resource.function.identity[0].principal_id
+}
