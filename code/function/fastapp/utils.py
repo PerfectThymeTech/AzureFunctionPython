c9ea9a2fad7781b5bffaf30946b07d5f90f3cba2
Marvin Buss <marvin.buss@gmail.com>
2023-06-14 16:12:55 +0200

    Add Open Telemetry

--- code/function/fastapp/utils.py
+++ code/function/fastapp/utils.py
@@ -1,7 +1,13 @@
 import logging
 from logging import Logger
 
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from fastapi import FastAPI
 from fastapp.core.config import settings
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 
 def setup_logging(module) -> Logger:
@@ -20,3 +26,18 @@ def setup_logging(module) -> Logger:
     )
     logger.addHandler(logger_stream_handler)
     return logger
+
+
+def setup_tracer(app: FastAPI):
+    """Setup tracer for Open Telemetry.
+
+    app (FastAPI): The app to be instrumented by Open Telemetry.
+    RETURNS (None): Nothing is being returned.
+    """
+    if settings.APPLICATIONINSIGHTS_CONNECTION_STRING:
+        exporter = AzureMonitorTraceExporter.from_connection_string(
+            settings.APPLICATIONINSIGHTS_CONNECTION_STRING
+        )
+        tracer = TracerProvider(resource=Resource({SERVICE_NAME: "api"}))
+        tracer.add_span_processor(BatchSpanProcessor(exporter))
+        FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
