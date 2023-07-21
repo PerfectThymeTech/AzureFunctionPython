2c5c25b4966817761149d3c8934b0b017e9b4fdf
Marvin Buss <marvin.buss@gmail.com>
2023-07-21 13:50:47 +0200

    Add role assignment

--- code/infra/roleassignments.tf
+++ code/infra/roleassignments.tf
@@ -1,3 +1,9 @@
+resource "azurerm_role_assignment" "current_role_assignment_key_vault" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azurerm_role_assignment" "function_role_assignment_storage" {
   scope                = azurerm_storage_account.storage.id
   role_definition_name = "Storage Blob Data Owner"
