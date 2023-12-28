8800b7b6b62d4e1f1c4df1619f03e36084a0c91f
Marvin Buss <marvin.buss@gmail.com>
2023-12-28 12:23:56 +0100

    Switch to Azure Monitor Library

--- code/function/fastapp/api/v1/endpoints/sample.py
+++ code/function/fastapp/api/v1/endpoints/sample.py
@@ -4,7 +4,6 @@ import aiohttp
 from fastapi import APIRouter, Header
 from fastapp.models.sample import SampleRequest, SampleResponse
 from fastapp.utils import setup_logging, setup_tracer
-from opentelemetry import trace
 
 logger = setup_logging(__name__)
 tracer = setup_tracer(__name__)
@@ -13,16 +12,12 @@ router = APIRouter()
 
 
 @router.post("/sample", response_model=SampleResponse, name="sample")
-async def post_predict(data: SampleRequest, x_forwarded_for: Annotated[str, Header()] = "") -> SampleResponse:
+async def post_predict(
+    data: SampleRequest, x_forwarded_for: Annotated[str, Header()] = ""
+) -> SampleResponse:
     logger.info(f"Received request: {data}")
 
     # Sample request
-    # async with aiohttp.ClientSession() as client:
-    #     async with client.get(url="https://www.bing.com/") as response:
-    #         resp_status_code = response.status
-    #         resp_text = await response.text()
-    # logger.info(f"Received response status code: {resp_status_code}")
-
     tracer_attributes = {"http.client_ip": x_forwarded_for}
     with tracer.start_as_current_span(
         "dependency_span", attributes=tracer_attributes
--- code/function/fastapp/utils.py
+++ code/function/fastapp/utils.py
@@ -2,27 +2,13 @@ import logging
 from logging import Logger
 
 # from azure.identity import ManagedIdentityCredential
-from azure.monitor.opentelemetry.exporter import (
-    AzureMonitorLogExporter,
-    AzureMonitorMetricExporter,
-    AzureMonitorTraceExporter,
-)
 from fastapi import FastAPI
 from fastapp.core.config import settings
 from opentelemetry import trace
-from opentelemetry._logs import set_logger_provider
 from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.system_metrics import SystemMetricsInstrumentor
-from opentelemetry.metrics import set_meter_provider
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.trace import Tracer, get_tracer_provider, set_tracer_provider
+from opentelemetry.trace import Tracer
+from azure.monitor.opentelemetry import configure_azure_monitor
 
 
 def setup_logging(module) -> Logger:
@@ -33,10 +19,6 @@ def setup_logging(module) -> Logger:
     logger = logging.getLogger(module)
     logger.setLevel(settings.LOGGING_LEVEL)
     logger.propagate = False
-
-    # Create logging handler
-    logging_handler = LoggingHandler()
-    logger.addHandler(logging_handler)
 
     # Create stream handler
     stream_handler = logging.StreamHandler()
@@ -65,37 +47,13 @@ def setup_opentelemetry(app: FastAPI):
     if settings.APPLICATIONINSIGHTS_CONNECTION_STRING:
         # credential = ManagedIdentityCredential()
 
-        # Create logger provider
-        logger_exporter = AzureMonitorLogExporter.from_connection_string(
-            settings.APPLICATIONINSIGHTS_CONNECTION_STRING,
-            # credential=credential
+        # Configure azure monitor exporter
+        configure_azure_monitor(
+            connection_string=settings.APPLICATIONINSIGHTS_CONNECTION_STRING,
+            disable_offline_storage=False,
+            # credential=credential,
         )
-        logger_provider = LoggerProvider()
-        logger_provider.add_log_record_processor(
-            BatchLogRecordProcessor(logger_exporter)
-        )
-        set_logger_provider(logger_provider)
-
-        # Create tracer provider
-        tracer_exporter = AzureMonitorTraceExporter.from_connection_string(
-            settings.APPLICATIONINSIGHTS_CONNECTION_STRING,
-            # credential=credential
-        )
-        tracer_provider = TracerProvider(resource=Resource({SERVICE_NAME: "api"}))
-        set_tracer_provider(tracer_provider)
-        get_tracer_provider().add_span_processor(BatchSpanProcessor(tracer_exporter))
-
-        # Create meter provider
-        metrics_exporter = AzureMonitorMetricExporter.from_connection_string(
-            settings.APPLICATIONINSIGHTS_CONNECTION_STRING,
-            # credential=credential
-        )
-        reader = PeriodicExportingMetricReader(
-            metrics_exporter, export_interval_millis=5000
-        )
-        meter_provider = MeterProvider(metric_readers=[reader])
-        set_meter_provider(meter_provider)
-
+        
         # Configure custom metrics
         system_metrics_config = {
             "system.memory.usage": ["used", "free", "cached"],
@@ -106,11 +64,5 @@ def setup_opentelemetry(app: FastAPI):
         }
 
         # Create instrumenter
-        FastAPIInstrumentor.instrument_app(
-            app,
-            excluded_urls=f"{settings.API_V1_STR}/health/heartbeat",
-            tracer_provider=tracer_provider,
-            meter_provider=meter_provider,
-        )
-        AioHttpClientInstrumentor().instrument(tracer_provider=tracer_provider)
+        AioHttpClientInstrumentor().instrument()
         SystemMetricsInstrumentor(config=system_metrics_config).instrument()
