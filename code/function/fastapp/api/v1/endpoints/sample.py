efbbd80e63aa0a83cf5d36615b5855278c341144
Marvin Buss <marvin.buss@gmail.com>
2023-12-28 18:08:40 +0100

    Test with span

--- code/function/fastapp/api/v1/endpoints/sample.py
+++ code/function/fastapp/api/v1/endpoints/sample.py
@@ -4,6 +4,7 @@ import httpx
 from fastapi import APIRouter, Header
 from fastapp.models.sample import SampleRequest, SampleResponse
 from fastapp.utils import setup_logging, setup_tracer
+from opentelemetry.trace import SpanKind
 
 logger = setup_logging(__name__)
 tracer = setup_tracer(__name__)
@@ -15,11 +16,19 @@ router = APIRouter()
 async def post_predict(
     data: SampleRequest, x_forwarded_for: Annotated[str, Header()] = ""
 ) -> SampleResponse:
-    logger.info(f"Received request: '{data}' from ip '{x_forwarded_for}'")
+    logger.info(f"Received request: {data}")
 
     # Sample request
-    async with httpx.AsyncClient() as client:
-        response = await client.get("https://www.bing.com")
-    logger.info(f"Received response status code: {response.status_code}")
+    tracer_attributes = {"http.client_ip": x_forwarded_for}
+    with tracer.start_as_current_span(
+        "dependency_span", attributes=tracer_attributes, kind=SpanKind.CLIENT
+    ) as span:
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get("https://www.bing.com")
+            logger.info(f"Received response status code: {response.status_code}")
+        except Exception as ex:
+            span.set_attribute("status", "exception")
+            span.record_exception(ex)
 
     return SampleResponse(output=f"Hello {data.input}")
