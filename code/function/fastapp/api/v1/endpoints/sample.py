b9c43acd7396e9eeca3b183a76b6085caf7a53ae
Marvin Buss <marvin.buss@gmail.com>
2024-01-01 17:48:56 +0100

    Add more requests

--- code/function/fastapp/api/v1/endpoints/sample.py
+++ code/function/fastapp/api/v1/endpoints/sample.py
@@ -23,6 +23,10 @@ async def post_predict(
         response = await client.get("https://www.bing.com")
     logger.info(f"Received response status code: {response.status_code}")
 
+    with httpx.Client() as client:
+        response = client.get("https://www.google.com")
+    response = httpx.get("https://www.google.de")
+
     # tracer_attributes = {"http.client_ip": x_forwarded_for}
     # with tracer.start_as_current_span(
     #     "dependency_span", attributes=tracer_attributes, kind=SpanKind.CLIENT
