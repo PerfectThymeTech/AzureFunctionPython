881aa85f76fdc98c14d3694fc693b5718c544c0d
Marvin Buss <marvin.buss@gmail.com>
2024-01-17 17:53:59 +0100

    Add dependency to health endpoint

--- code/function/fastapp/api/v1/endpoints/heartbeat.py
+++ code/function/fastapp/api/v1/endpoints/heartbeat.py
@@ -1,6 +1,7 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapp.health.validate_request import verify_health_auth_header
 from fastapp.models.heartbeat import HearbeatResult
 from fastapp.utils import setup_logging
 
@@ -9,7 +10,12 @@ logger = setup_logging(__name__)
 router = APIRouter()
 
 
-@router.get("/heartbeat", response_model=HearbeatResult, name="heartbeat")
+@router.get(
+    "/heartbeat",
+    response_model=HearbeatResult,
+    name="heartbeat",
+    dependencies=[Depends(verify_health_auth_header)],
+)
 async def get_hearbeat() -> Any:
     logger.info("Received Heartbeat Request")
     return HearbeatResult(isAlive=True)
